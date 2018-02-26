3aca024f1445c8aebdb803dbcb33d87e4ce62c77
Phani rama krishna <phani.r@msystechnologies.com>
2018-02-26 12:42:14 +0530

    Deploy file changes

--- config/puma.rb
+++ config/puma.rb
@@ -22,7 +22,7 @@ environment ENV.fetch("RAILS_ENV") { "development" }
 # processes).
 #
 # workers ENV.fetch("WEB_CONCURRENCY") { 2 }
- bind "unix:///home/contactbook/shared/tmp/sockets/puma.sock"
+ bind "unix:///home/deploy/contactbook/shared/tmp/sockets/puma.sock"
 
 # Use the `preload_app!` method when specifying a `workers` number.
 # This directive tells Puma to first boot the application and load code
